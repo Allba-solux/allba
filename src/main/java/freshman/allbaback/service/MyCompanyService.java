f7979547b74c118f273b554f818ba7ca6e590241
hancrysta1 <hancrystal@sookmyung.ac.kr>
2023-02-04 23:47:48 +0900

    [ADD] 지점 참가

---
+++ src/main/java/freshman/allbaback/service/MyCompanyService.java
@@ -0,0 +1,39 @@
+package freshman.allbaback.service;
+
+import freshman.allbaback.domain.MyCompany;
+import freshman.allbaback.domain.members.Members;
+import freshman.allbaback.repository.MembersRepository;
+import freshman.allbaback.repository.MyCompanyRepository;
+import freshman.allbaback.web.dto.MembersSaveRequestDto;
+import freshman.allbaback.web.dto.MyCompanySaveRequestDto;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MyCompanyService {
+    private final MyCompanyRepository myCompanyRepository;
+
+    public MyCompanyService(MyCompanyRepository myCompanyRepository) {
+        this.myCompanyRepository = myCompanyRepository;
+    }
+
+
+    @Transactional
+    public String save(MyCompanySaveRequestDto requestDto) {
+        return myCompanyRepository.save(requestDto.toEntity()).getPid();
+    }
+
+    public List<MyCompany> findByPid(String pid) {
+        return myCompanyRepository.findByPid(pid);
+    }
+    public Optional<Object> findByCompanyName(String companyName) {
+        return myCompanyRepository.findByCompanyName(companyName);
+    }
+    public List<MyCompany> findMembers() {
+        return myCompanyRepository.findAll();
+    }
+
+}
