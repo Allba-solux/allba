e4d8489a468a7987afb551a2c5f8f535332eb321
hancrysta1 <hancrystal@sookmyung.ac.kr>
2023-02-09 02:43:25 +0900

    [ADD/FIX] HomeController 추가, 로그인 세션 보완

--- src/main/java/freshman/allbaback/service/LoginService.java
+++ src/main/java/freshman/allbaback/service/LoginService.java
@@ -5,15 +5,14 @@ import freshman.allbaback.repository.MembersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+
 @RequiredArgsConstructor
 @Service
 public class LoginService {
+
     private final MembersRepository membersRepository;
-
-    public Members login(String pid, String pwd) {
-
-        return membersRepository.findByLoginPid(pid)
-                .filter(m -> m.getPwd().equals(pwd))
-                .orElse(null);//로그인 실패
+    public Members loginMem(String pid, String pwd) {
+        System.out.println(pid+"\n"+pwd);
+        return membersRepository.findByPid(pid).filter(m -> m.getPwd().equals(pwd)).orElse(null);//.orElse(null)
     }
 }
